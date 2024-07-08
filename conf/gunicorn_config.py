9551a6546db50d4bfcb672681a0fac2400c8a666
xexeyt5 <cbwyuren500@gmail.com>
2024-07-08 13:28:54 -0600

    change

---
+++ conf/gunicorn_config.py
@@ -0,0 +1,5 @@
+command = '/home/xeyt/residencia-proyect/venv/bin/gunicorn'
+pythonpath = '/home/xeyt/residencia-proyect/Inventario'
+bind = '192.168.1.77:8000'
+workers = 3
+
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/xeyt/Documents/residencia-proyect/venv"
+set -gx VIRTUAL_ENV "/home/xeyt/residencia-proyect/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
